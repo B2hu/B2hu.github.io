26d0f0f336ed77030574f4749132b5807841a5c0
B2hu <ahmedbh.professional@gmail.com>
2025-01-18 03:18:36 +0100

    google analytics

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737165034',resources: [
+    cacheName: 'chirpy-1737165455',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
